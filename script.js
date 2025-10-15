148dff1d0ae1882b71871e5945666cca83b2736c
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-15 23:50:09 +0545

    Implement drag-and-drop functionality with event listeners for list items

---
+++ script.js
@@ -0,0 +1,26 @@
+let lists = document.querySelectorAll('.list');
+let right = document.getElementById('right');
+let left = document.getElementById('left');
+
+for(list of lists){
+    list.addEventListener('dragstart', function(e){
+        let selected = e.target;
+
+        right.addEventListener('dragover', function(e){
+            e.preventDefault();
+        });
+        
+        right.addEventListener('drop', function(e){
+            right.appendChild(selected);
+            selected = null;
+        });
+        left.addEventListener('dragover', function(e){
+            e.preventDefault();
+        });
+        
+        left.addEventListener('drop', function(e){
+            left.appendChild(selected);
+            selected = null;
+        });
+    });
+}
